7123c106a4128821da5e44bbc81d31a97efe2eaf
David Zuccaro <zuccaro.david@gmail.com>
2017-03-20 00:05:09 +1100

    add rainfall column chart

--- clim-av.py
+++ clim-av.py
@@ -6,7 +6,7 @@
 
 import matplotlib
 import matplotlib.pyplot as plt
-#matplotlib.use('TkAgg') 
+matplotlib.use('TkAgg') 
 
 import matplotlib.ticker as ticker
 
@@ -18,15 +18,32 @@ import mysql.connector
 
 import argparse
 
+import matplotlib.patches as patches
+
+import matplotlib.path    as path
+
 degree_sign 	    = unichr(176)
 char_inc            = 0
 temp_tots           = []
+precip_tots         = []
 avesl               = np.zeros(365)
 avesh               = np.zeros(365)
+precip_aves         = np.zeros(12)
 mypath              = "/srv/clim-data/ghcnd_temp/"
 temp                = 0.0
+min_col             = '#eac220'
+max_col             = '#a30b0b'
+rain_col            = 'blue'
 
 month_days          = (31,28,31,30,31,30,31,31,30,31,30,31)
+month_axes          = np.zeros(13)
+
+month_axes[0] = 0
+tot = 0
+for i in range(12):
+    tot += month_days[i]
+    month_axes[i+1] = tot    
+
 start_temp          = 0
 end_temp            = 0
 
@@ -35,7 +52,7 @@ grids               = np.zeros(12)
 
 parser 				= argparse.ArgumentParser()
 
-parser.add_argument("-s",  "--station",  default="N00076077", help="ghcn weather station code")
+parser.add_argument("-s",  "--station",  default="USC00464836", help="ghcn weather station code")
 parser.add_argument("-f",  "--outfile",  default="test.png" , help="the output filename"      )
 
 args = parser.parse_args()
@@ -48,7 +65,7 @@ cnx = mysql.connector.connect(user='root', password='happy1', database='ghcndata
 cursor1 = cnx.cursor(buffered=True)
 cursor2 = cnx.cursor()
 cmd = "SELECT station_name, latitude, longitude FROM station WHERE field1 = '" + args.station[2:] + "' AND country_code = '" + args.station[:2] + "';"
-print cmd
+
 get_station = (cmd)
 
 cursor1.execute(get_station)
@@ -57,8 +74,6 @@ station_data = []
 
 for (record) in cursor1:
     station_data.append(record)
-
-print station_data
 
 cmd = "SELECT name FROM country WHERE code = '" + args.station[:2] + "';"
 get_country = (cmd)
@@ -97,8 +112,12 @@ for i in range(1,13):
 
 # set up main data array to calc aves
 for x in range(365):
-    myline = [0,0,0,0]
+    myline = [0.0, 0.0, 0.0, 0.0]
     temp_tots.append(myline)
+
+for x in range(12):
+    myline = [0.0, 0.0]
+    precip_tots.append(myline)
 
 #loop through each line of file
 with open(fullpath, "r") as this_file:
@@ -153,31 +172,83 @@ with open(fullpath, "r") as this_file:
                 else:
                     break
                     
+        #look for precipitation
+        if line[17:21] == "PRCP":
+            this_month = int(line[15:17]) 
+
+            this_year = int(line[11:15]) # get year        
+            
+            char_inc = 0
+            
+            for col in range(month_days[this_month - 1]):      # scan thru days on each line  
+  
+                start_precip = 21 + char_inc
+                end_precip   = 21 + char_inc + 5             # isolate precip reading
+
+                if end_precip < len(line):                 # check for end of line (days < 31)
+                    precip = float(line[start_precip:end_precip])
+                    
+                    if precip >= 0:      # ignore invalid precipitation                        
+                        precip_tots[this_month - 1][0] += 1
+                        precip_tots[this_month - 1][1] += float(precip) 
+                        char_inc += 8                       # move to next precipitation
+                else:
+                    break
+                    
 #calculate aves
 i = 0
-for x in temp_tots:
+for x in temp_tots:    
     if x[0] > 0:
-        avesl[i] = x[1] / (x[0] * 10) #mins
-        avesh[i] = x[3] / (x[2] * 10) #maxes
+        avesl[i]  = x[1] / (x[0] * 10) #mins
+    if x[2] > 0:
+        avesh[i]  = x[3] / (x[2] * 10) #maxes
     i += 1
+    
+i = 0
+for x in precip_tots:
+    if x[0] > 0:
+        a = month_days[i] * x[1] / (x[0] * 10) #precipation        
+        precip_aves[i] = a
         
-# create graph
+    i += 1
+    
+# set up precipitation bar graph
+
+left    = np.array(month_axes[:-1])      
+right   = np.array(month_axes[1:])
+bottom  = np.zeros(len(left))
+top     = bottom + precip_aves
+
+# create temp graph
 
 #lines
 fig2 = plt.figure(figsize=(22.0, 9.5)) # The size of the figure is specified as (width, height) in inches
 
-# 7 degree line of best fit
+# we need a (numrects x numsides x 2) numpy array for the path helper
+# function to build a compound path
+XY = np.array([[left, left, right, right], [bottom, top, top, bottom]]).T
+
+# get the Path object
+barpath = path.Path.make_compound_path_from_polys(XY)
+
+# make a patch out of it
+patch = patches.PathPatch(barpath, facecolor=rain_col, edgecolor='gray', alpha=0.5)
+fig2.add_subplot(111).add_patch(patch) 
+
+# n degree line of best fit
 l1 = fig2.add_subplot(111).plot(range(365), np.poly1d(np.polyfit(range(365), avesh, 5))(range(365)), 
-                                label="Maximums", lw=10, color='red')
+                                label="Maximums", lw=10, color=max_col)
 
 l2 = fig2.add_subplot(111).plot(range(365), np.poly1d(np.polyfit(range(365), avesl, 5))(range(365)), 
-                                label="Minimums", lw=10, color='blue')
+                                label="Minimums", lw=10, color=min_col)
                                 
-l3 = fig2.add_subplot(111).plot(range(365), avesh, 'ro')
+l3 = fig2.add_subplot(111).plot(range(365), avesh, 'o', color=max_col)
 
-l4 = fig2.add_subplot(111).plot(range(365), avesl, 'bo')
-
-fig2.add_subplot(111).legend(("Maximums", "Minimums"), loc=0)
+l4 = fig2.add_subplot(111).plot(range(365), avesl, 'o', color=min_col)
+red_patch  = patches.Patch(color=max_col,  label='Average Maximum Temperature')
+yel_patch  = patches.Patch(color=min_col,  label='Average Minimum Temperature')
+blue_patch = patches.Patch(color=rain_col, label='Rainfall', alpha=0.5)
+plt.legend(handles=[red_patch, yel_patch, blue_patch], loc=0)
 
 #axes
 fig2.add_subplot(111).set_xticks(grids, minor=True)
@@ -223,3 +294,5 @@ image = "/var/www/html/clim-data/images/" + outfn
 
 #print image
 fig2.savefig(image)
+
+fig2.show()
